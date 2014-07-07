e44eb6a430473f722c8d430e6cd39c32b9980b16
Anton Schukin <palich@yandex-team.ru>
2014-07-07 09:52:15 +0400

    Updating Flurry SDK to 5.2.0

---
+++ Flurry API Documentation/dir_62935c1d3d87d3cf11039c2f548b328d.js
@@ -0,0 +1,5 @@
+var dir_62935c1d3d87d3cf11039c2f548b328d =
+[
+    [ "Flurry", "dir_df2c7d4721d72069379f16dd06b649da.html", "dir_df2c7d4721d72069379f16dd06b649da" ],
+    [ "FlurryAds", "dir_1a6000fbaa0f3b34a99cc33c3be07409.html", "dir_1a6000fbaa0f3b34a99cc33c3be07409" ]
+];
--- Flurry API Documentation/dir_befff70f4aff068090eb45949a2e3fc4.js
+++
@@ -1,4 +0,0 @@
-var dir_befff70f4aff068090eb45949a2e3fc4 =
-[
-    [ "Flurry.h", "_flurry_8h_source.html", null ]
-];
